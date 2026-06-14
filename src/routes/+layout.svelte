48c0a20adec947df85fb267aa34ca6cca8cb51d3
Michael Obubelebra Amachree <amachree9630@gmail.com>
2026-06-14 11:01:10 +0100

    feat: add Toaster component for improved user notifications

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,14 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import { ModeWatcher } from 'mode-watcher';
+	import { Toaster } from '$lib/components/ui/sonner/index.js';
 	import Navbar from '$lib/components/navbar.svelte';
 
 	let { children } = $props();
 </script>
 
 <ModeWatcher />
+<Toaster />
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
 <Navbar />
