4ca24f1b0273f69c0d712491416b171cd6bb2aa0
Michael Obubelebra Amachree <amachree9630@gmail.com>
2026-06-15 12:04:10 +0100

    feat: add Svelte configuration for Netlify adapter and Vite plugin settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [
-		tailwindcss(),
-		sveltekit({
-			compilerOptions: {
-				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-				runes: ({ filename }) =>
-					filename.split(/[/\\]/).includes('node_modules') ? undefined : true,
-				experimental: { async: true }
-			},
-			experimental: { remoteFunctions: true, handleRenderingErrors: true },
-			vitePlugin: {
-				inspector: {
-					toggleKeyCombo: 'alt-x',
-					showToggleButton: 'always',
-					toggleButtonPos: 'bottom-left'
-				}
-			}
-		})
-	]
+	plugins: [tailwindcss(), sveltekit()]
 });
